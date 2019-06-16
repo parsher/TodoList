376a52dde7a3d5ea7e6308b8acbaed447a9f17c6
parsher <futureslabor@gmail.com>
2019-06-16 21:07:40 +0900

    Initial commit

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.todolist
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import io.realm.Realm
+import io.realm.Sort
+import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import org.jetbrains.anko.startActivity
+
+class MainActivity : AppCompatActivity() {
+    val realm = Realm.getDefaultInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        val realmResult = realm.where<Todo>().findAll().sort("date", Sort.DESCENDING)
+
+        val adapter = TodoListAdapter(realmResult)
+        listView.adapter = adapter
+
+        realmResult.addChangeListener { _ -> adapter.notifyDataSetChanged() }
+
+        listView.setOnItemClickListener { parent, view, position, id  ->
+            startActivity<EditActivity>("id" to id)
+        }
+
+        fab.setOnClickListener {
+            startActivity<EditActivity>()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
